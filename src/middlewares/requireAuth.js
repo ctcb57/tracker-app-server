f12b1c6cd9c71fa270e2b38704401537dbb62c4a
charles clark <charles@pop-os.localdomain>
2020-08-19 01:18:08 -0500

    Added tracks schema, post, and get routes

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization) {
-        return res.states(401).send({ error: 'You must be logged in.' });
+        return res.status(401).send({ error: 'You must be logged in.' });
     }
 
     const token = authorization.replace('Bearer ', '');
